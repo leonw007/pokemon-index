0974418e7dfce34addc549cdc48d333a6cf28af1
Chen Wang <leonw007@gmail.com>
2015-11-07 00:23:49 -0500

    model pokemon initialize

---
+++ pokemon-by-Leon/Pokemon.swift
@@ -0,0 +1,29 @@
+//
+//  Pokemon.swift
+//  pokemon-by-Leon
+//
+//  Created by Chen Wang on 11/7/15.
+//  Copyright © 2015 Chen Wang. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init (name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+    
+    
+}
