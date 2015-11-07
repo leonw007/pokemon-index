5e2e3432b9fc9f6e4b1d72faf97aef1987e1858c
Chen Wang <leonw007@gmail.com>
2015-11-07 14:27:46 -0500

    custom collection view

---
+++ pokemon-by-Leon/PokeCell.swift
@@ -0,0 +1,13 @@
+//
+//  PokeCell.swift
+//  pokemon-by-Leon
+//
+//  Created by Chen Wang on 11/7/15.
+//  Copyright © 2015 Chen Wang. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+}
