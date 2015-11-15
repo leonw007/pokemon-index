b4dd3e35865acd26384b53642913acf04c74b097
Chen Wang <leonw007@gmail.com>
2015-11-14 21:59:22 -0500

    add search bar

--- pokemon-by-Leon/ViewController.swift
+++ pokemon-by-Leon/ViewController.swift
@@ -8,17 +8,23 @@
 
 import UIKit
 
-class ViewController:  UIViewController, UICollectionViewDelegate,UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+class ViewController:  UIViewController, UICollectionViewDelegate,UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UISearchBarDelegate {
     
     @IBOutlet weak var collection: UICollectionView!
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    
     var pokemons = [Pokemon]()
+    var filteredPokemon = [Pokemon]()
+    var inSearchMode = false
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
         collection.delegate = self
         collection.dataSource = self
+        searchBar.delegate = self
+        searchBar.returnKeyType = UIReturnKeyType.Done
         
         parsePokemonCSV()
         
@@ -55,7 +61,15 @@ class ViewController:  UIViewController, UICollectionViewDelegate,UICollectionVi
         if let cell = collectionView.dequeueReusableCellWithReuseIdentifier("PokeCell", forIndexPath: indexPath) as? PokeCell{
             
             
-            let pokemon = pokemons[indexPath.row]
+            let pokemon: Pokemon!
+            
+            if inSearchMode {
+                pokemon = filteredPokemon[indexPath.row]
+            } else {
+                pokemon = pokemons[indexPath.row]
+            }
+            
+            
             
             cell.configCell(pokemon)
             
@@ -72,7 +86,13 @@ class ViewController:  UIViewController, UICollectionViewDelegate,UICollectionVi
     
     //number of itmes in one section
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 718
+        
+        if inSearchMode {
+            return filteredPokemon.count
+        } else {
+            return pokemons.count
+        }
+        
     }
     
     func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
@@ -84,6 +104,36 @@ class ViewController:  UIViewController, UICollectionViewDelegate,UICollectionVi
     func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
         return CGSizeMake(105, 105)
     }
+    
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        view.endEditing(true)
+    }
+    
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        if searchBar.text == nil || searchBar.text == "" {
+            inSearchMode = false
+            //disappear the key board when there is no words
+            view.endEditing(true)
+            collection.reloadData()
+        } else {
+            inSearchMode = true
+            let lower = searchBar.text!.lowercaseString
+            //check whether what user typed is in our list
+            filteredPokemon = pokemons.filter({$0.name.rangeOfString(lower) != nil})
+            collection.reloadData()
+            
+            
+        
+        }
+        
+    }
+    
+    
+    
+    
+    
+    
     
 
 }
